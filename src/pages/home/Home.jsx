ea061e233a9c2b6d2097936576dc245846a58500
anagpure28 <aniketnagpureofficial@gmail.com>
2023-09-13 01:11:33 +0530

    TopRated Carousel & home done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import "./style.scss"
 import HeroBanner from './heroBanner/HeroBanner';
 import Trending from './trending/Trending';
 import Popular from './popular/Popular';
+import TopRated from './topRated/TopRated';
 
 export const Home = () => {
   return (
@@ -10,7 +11,7 @@ export const Home = () => {
         <HeroBanner />
         <Trending />
         <Popular />
-        <div style={{ height: "1000px"}}></div>
+        <TopRated />
     </div>
   )
 }
