f986848e4a2f5db893c682a4f548ca464c56f9fd
anagpure28 <aniketnagpureofficial@gmail.com>
2023-09-13 00:53:28 +0530

    Carousel side button

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -21,8 +21,15 @@ const Carousel = ({data, loading}) => {
   const navigate = useNavigate();
 
   const navigation = (dir) => {
+    const container = carouselContainer.current;
 
-  }
+    const scrollAmount = dir === "left" ? container.scrollLeft - (container.offsetWidth + 20) : container.scrollLeft + (container.offsetWidth + 20);
+    container.scrollTo({
+        left: scrollAmount,
+        behavior: "smooth"
+    })
+
+}
 
   const skItem = () => {
     return (
@@ -49,13 +56,13 @@ const Carousel = ({data, loading}) => {
                 onClick={()=> navigation("right")}
             />
             {!loading ? (
-                <div className="carouselItems">
+                <div className="carouselItems" ref={carouselContainer}>
                     {data?.map((item) => {
                         const posterUrl = item.poster_path 
                             ? url.poster + item.poster_path
                             : PosterFallback; 
                         return (
-                            <div key={item.id} className="carouselItem">
+                            <div key={item.id} className="carouselItem" onClick={() => navigate(`/${item.media_type}/${item.id}`)}>
                                 <div className="posterBlock">
                                     <Img src={posterUrl} />
                                     <CircleRating rating={item.vote_average.toFixed(1)} />
