cbc5e6775fc3fd2f00baee468b4ef19eaf04459c
anagpure28 <aniketnagpureofficial@gmail.com>
2023-09-13 01:07:10 +0530

    Popular Carousel added

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -14,7 +14,7 @@ import PosterFallback from "../../assets/no-poster.png";
 import CircleRating from "../circleRating/CircleRating";
 import Genres from "../genres/Genres";
 
-const Carousel = ({data, loading}) => {
+const Carousel = ({data, loading, endpoint}) => {
 
   const carouselContainer = useRef();
   const {url} = useSelector((state)=> state.home);
@@ -62,7 +62,7 @@ const Carousel = ({data, loading}) => {
                             ? url.poster + item.poster_path
                             : PosterFallback; 
                         return (
-                            <div key={item.id} className="carouselItem" onClick={() => navigate(`/${item.media_type}/${item.id}`)}>
+                            <div key={item.id} className="carouselItem" onClick={() => navigate(`/${item.media_type || endpoint}/${item.id}`)}>
                                 <div className="posterBlock">
                                     <Img src={posterUrl} />
                                     <CircleRating rating={item.vote_average.toFixed(1)} />
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import "./style.scss"
 import HeroBanner from './heroBanner/HeroBanner';
 import Trending from './trending/Trending';
+import Popular from './popular/Popular';
 
 export const Home = () => {
   return (
     <div className='HomePage'>
         <HeroBanner />
         <Trending />
+        <Popular />
         <div style={{ height: "1000px"}}></div>
     </div>
   )
