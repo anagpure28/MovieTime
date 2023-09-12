e959f81f31d19b9ba732dcb01dcc58451817386b
anagpure28 <aniketnagpureofficial@gmail.com>
2023-09-13 01:59:41 +0530

    SingleMoviePage overview done

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import "./style.scss"
+import { useParams } from 'react-router-dom';
+import DetailsBanner from './detailsBanner/DetailsBanner';
 
 export const Details = () => {
+  // const {mediaType, id} = useParams();
+  // const {data, loading} = useFetch(`/${mediaType}/${id}`)
+
   return (
-    <div>Details</div>
+    <div>
+      <DetailsBanner />
+    </div>
   )
 }
