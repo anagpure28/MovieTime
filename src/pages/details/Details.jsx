1204b137a2c0f4774d98df94e22748bc64070b27
anagpure28 <aniketnagpureofficial@gmail.com>
2023-09-14 01:21:03 +0530

    Cast added

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,7 @@ import "./style.scss"
 import { useParams } from 'react-router-dom';
 import useFetch from "../../hooks/useFetch";
 import DetailsBanner from './detailsBanner/DetailsBanner';
+import Cast from './cast/Cast';
 
 export const Details = () => {
   const {mediaType, id} = useParams();
@@ -12,6 +13,7 @@ export const Details = () => {
   return (
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
     </div>
   )
 }
